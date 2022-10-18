6d94df023abd8516b95850cbdf44a872c2923a1b
Raul Smith <raul.n.smith@gmail.com>
2022-10-17 19:02:25 -0600

    Modified Privacy Page and added tinymce

--- pages/privacy/index.js
+++ pages/privacy/index.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 import swal from 'sweetalert';
 
 export default function Home() {
   const [content, setContent] = useState('');
+  const editorRef = useRef(null);
 
   useEffect(() => {
     async function fetchData() {
@@ -21,28 +23,49 @@ export default function Home() {
 
   const handleFormSubmit = e => {
     e.preventDefault();
-    axios({
-      method: 'post',
-      url: 'https://galacticblue.net/cheekyginger/backend/public/api/privacy-pages/1',
-      headers: { 'content-type': 'application/json' },
-      data: { 'description': content },
-      params: {
-        '_method': 'PUT'
-      }
+    if (editorRef.current) {
+      setContent(editorRef.current.getContent());
+    }
+    setTimeout(() => {
+      axios({
+        method: 'post',
+        url: 'https://galacticblue.net/cheekyginger/backend/public/api/privacy-pages/1',
+        headers: { 'content-type': 'application/json' },
+        data: { 'text': editorRef.current.getContent() },
+        params: {
+          '_method': 'PUT'
+        }
+      })
+      .then(result => {
+        swal("Success!", "Privacy page content successfully updated!", "success")
+      })
+      .catch(error => swal("Uh oh! Something went wrong. Please try again."))
     })
-    .then(result => {
-      swal("Success!", "Privacy page content successfully updated!", "success")
-    })
-    .catch(error => swal("Uh oh! Something went wrong. Please try again."))
   }
 
   return (
     <div>
       <h1>Privacy Page Content</h1>
-      <form className="border border-primary px-2 py-2" onSubmit={e => handleFormSubmit(e)}>
+      <form className="border border-primary px-2 py-2" onSubmit={handleFormSubmit}>
         <div className="mb-3">
-          <label htmlFor='contentText' className="form-label">Page Content</label>
-          <textarea className="form-control" id="contentText" name="contentText" required value={content} onChange={e => setContent(e.target.value)} rows="5" />
+          <Editor
+            onInit={(evt, editor) => editorRef.current = editor}
+            initialValue={content}
+            init={{
+              height: 900,
+              menubar: false,
+              plugins: [
+                'advlist autolink lists link image charmap print preview anchor',
+                'searchreplace visualblocks code fullscreen',
+                'insertdatetime media table paste code help wordcount'
+              ],
+              toolbar: 'undo redo | formatselect | ' +
+              'bold italic backcolor | alignleft aligncenter ' +
+              'alignright alignjustify | bullist numlist outdent indent | ' +
+              'removeformat | help',
+              content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+            }}
+          />
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
